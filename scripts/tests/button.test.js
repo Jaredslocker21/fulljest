85af9e989180d21a4051bad17d3c6af3c4e526aa
Jaredslocker21 <jaredslocker21@gmail.com>
2022-09-07 11:36:00 +0000

    all

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,21 +1,17 @@
-const buttonClick = require("../buttons");
+/**
+ * @jest-environment jsdom
+ */
 
-beforeEach(() => {
-    let fs = require("fs")
-    let fileContents = fs.readFileSync("index.html", "utf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
+ const buttonClick = require("../button");
 
-});
-
-describe("DOM tests", () => {
-    test("Expects content to change", () => {
-        buttonClick(); 
-        expect(document.getElementById("par")
-            .innerHTML).toEqual("You Clicked");
-    });
-    test("h1 should exist", () =>{
-        expect(document.getElementsByTagName("h1").length).toBe(1)
-    });
-});
+ beforeAll(() => {
+     document.body.innerHTML = "<p id='par'></p>";
+ });
+ 
+ describe("DOM tests", () => {
+     test("Expects content to change", () => {
+         buttonClick();
+         expect(document.getElementById("par")
+             .innerHTML).toEqual("You Clicked");
+     });
+ });
